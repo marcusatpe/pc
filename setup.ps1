94545e51ba980c3dc18f595f8b9b9367e813e2a9
Marcus Mills <marcus.mills@practiceevolve.com>
2022-08-16 09:37:55 +0100

    Initial setup

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+# Run in elevated prompt
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install apps
+choco install vscode git poshgit sharex -y
